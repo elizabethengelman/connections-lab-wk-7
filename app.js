385a4b77a6cc76bf87442ac36f9f92ab7b487483
Elizabeth Engelman <elizabeth@redsquirrel.com>
2022-10-25 19:05:29 -0400

    Save the uploaded image into the db

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
 
+/*-----Middleware------*/
 app.use(
   fileUpload({
     createParentPath: true,
@@ -13,17 +14,22 @@ app.use(
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+/*-----Database------*/
+let datastore = require("nedb");
+let db = new datastore("trees.db");
+db.loadDatabase();
+
+/*-----Routes------*/
 app.post("/upload", (req, res) => {
   if (!req.files) {
     res.send({ success: false, message: "No file uploaded" });
   }
   let file = req.files.filetoupload;
-  file.mv("./uploads/" + file.name);
+  db.insert(file);
   res.send({ success: true, message: "File uploaded" });
 });
 
