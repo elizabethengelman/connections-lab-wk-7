5b51bd49dd01e7bfdcad4854922f22068008f4e1
Elizabeth Engelman <elizabeth@redsquirrel.com>
2022-10-25 19:01:06 -0400

    Upload files sync via a form

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
 app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/upload", (req, res) => {
+  if (!req.files) {
+    res.send({ success: false, message: "No file uploaded" });
+  }
+  let file = req.files.filetoupload;
+  file.mv("./uploads/" + file.name);
+  res.send({ success: true, message: "File uploaded" });
 });
 
 app.listen(port, () => {
